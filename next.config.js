633bff5edbff65092a8370ee19444a48c7731105
miltondw <estradamilton2001@gmail.com>
2021-06-11 18:22:59 -0500

    created login part one

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
     BASE_URL: "http://localhost:3000",
     MONGODB_URL:
       "mongodb+srv://miltondw:6N3ZMnJs46qTAG2t@cluster0.s2fjm.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "`S:&xbffA5LFD<!e5SptY^#w!,B!ajz`>^?%u',z`D\4zx%t",
+    REFRESH_TOKEN_SECRET:
+      "'RaR!;V:BCYme>Z+p<k_2ntu8mM,J^'!!8K!$>TE-QwZ?pPw`QfykhwUeMSQ6@MC+2kZMdT:T",
   },
 };
--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,12 +1,61 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState, useContext } from "react";
+import { DataContext } from "../../store/GlobalState";
+import { postData } from "../../utils/fetchData";
+import Cookies from "js-cookie";
 export default function Signin() {
+  const initialState = { email: "", password: "" };
+
+  const [userData, setUserData] = useState(initialState);
+
+  const { email, password } = userData;
+
+  const { state, dispatch } = useContext(DataContext);
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setUserData({ ...userData, [name]: value });
+
+    dispatch({ type: "NOTIFY", payload: {} });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (errMsg) return dispatch({ type: "NOTIFY", payload: { error: errMsg } });
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+
+    const res = await postData("auth/register", userData);
+
+    if (res.err)
+      return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+
+    dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+    dispatch({
+      type: "AUTH",
+      payload: {
+        toke: res.access_token,
+        user: res.user,
+      },
+    });
+
+    Cookies.set("refreshtoken", res.refresh_token, {
+      path: "api/auth/accesToken",
+      expires: 7,
+    });
+  };
   return (
     <div>
       <Head>
         <title>Sig in page</title>
       </Head>
-      <form className="mx-auto my-4" style={{ maxWidth: "500px" }}>
+      <form
+        className="mx-auto my-4"
+        style={{ maxWidth: "500px" }}
+        onSubmit={handleSubmit}
+      >
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
@@ -16,6 +65,9 @@ export default function Signin() {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            name="email"
+            value={email}
+            onChange={handleChangeInput}
           />
         </div>
         <div className="mb-3">
@@ -26,6 +78,9 @@ export default function Signin() {
             type="password"
             className="form-control"
             id="exampleInputPassword1"
+            name="password"
+            value={password}
+            onChange={handleChangeInput}
           />
         </div>
         <button type="submit" className="btn btn-primary w-100">
