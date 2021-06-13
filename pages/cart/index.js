d896a9158425ca261906c87ce3b38c3977aaf2f3
miltondw <estradamilton2001@gmail.com>
2021-06-12 19:18:50 -0500

    Created cart part two

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useContext } from "react";
+import { DataContext } from "../../store/GlobalState";
+import Head from "next/head";
+import CartItem from "../../components/CartItem/CartItem";
 
 export default function Cart() {
-    return (
-        <div>
-            Cart
-        </div>
-    )
+  const { state, dispatch } = useContext(DataContext);
+  const { cart } = state;
+  if (cart.length === 0) return <h2>No products</h2>;
+  return (
+    <div className="row mx-auto">
+      <Head>
+        <title>Cart Page</title>
+      </Head>
+      <div className="col-md-8 text-secondary table-responsive my-3">
+        <h2 className="text-uppercase">shopping cart</h2>
+        <table className="table my-3">
+          <tbody>
+            {cart.map((item) => (
+              <CartItem
+                key={item._id}
+                item={item}
+                dispatch={dispatch}
+                cart={cart}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="col-md-4"></div>
+    </div>
+  );
 }
