14550fac3f13511798838d44418f25efc00d9dac
miltondw <estradamilton2001@gmail.com>
2021-07-02 13:43:53 -0500

    Service from images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images: {
-    domains: ["res.cloudinary.com", "localhost", "blob:http://localhost"],
+    domains: [
+      "res.cloudinary.com",
+      "localhost",
+      "blob:http://localhost",
+      "https://e-commerce-nextjs-kappa.vercel.app/",
+    ],
   },
 };
