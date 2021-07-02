406602b85be939089acc0410cfb9a39d4d16fe7c
miltondw <estradamilton2001@gmail.com>
2021-07-02 13:35:30 -0500

    experiment with routes

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,6 +1,16 @@
 const baseUrl = process.env.BASE_URL;
 
 export const getData = async (url, token) => {
+  if(url==="categories"){
+    const res = await fetch(`/api/${url}`, {
+      method: "GET",
+      headers: {
+        "Authorization": token,
+      },
+    });
+    const data = await res.json();
+    return data;
+  }
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: "GET",
     headers: {
